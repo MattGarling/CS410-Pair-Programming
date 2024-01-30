ae102158d88f29f63704fa807ef4c30f91dc6558
MattGarling <matthewgarling@gmail.com>
2024-01-30 11:49:02 -0600

    Added Betting and changed the end of hand logic

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -11,6 +11,7 @@ import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -54,19 +55,19 @@ public class BlackJack {
 	// dealer
 	Card hiddenCard; // the dealer's hidden card
 	ArrayList<Card> dealerHand; // what cards the dealer has
-	int dealerSum; // the sum of the cards in the dealers hand
+	int dealerHandSum; // the sum of the cards in the dealers hand
 	int dealerAceCount; // the count of aces in the dealers hand
 
 	// player
 	ArrayList<Card> playerHand; // what cards the player has
-	int playerSum; // the sum of the cards in the players hand
+	int playerHandSum; // the sum of the cards in the players hand
 	int playerAceCount; // the count of aces in the players hand
 	int playerChips; // the total count of the player's chips
 	int playerBet; // the amount of chips the user has bet on the hand
 
 	// window
-	int boardWidth = 600; // set the width of the board
-	int boardHeight = boardWidth; // set the height of the board
+	int boardWidth = 1000; // set the width of the board
+	int boardHeight = 600; // set the height of the board
 
 	int cardWidth = 110; // set the width of the cards
 	int cardHeight = 154; // set the height of the cards
@@ -78,44 +79,61 @@ public class BlackJack {
 			super.paintComponent(g);
 
 			try {
-				// draw hidden card
-				Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
-				if (!stayButton.isEnabled()) {
-					hiddenCardImg = new ImageIcon(getClass().getResource(hiddenCard.getImagePath())).getImage();
+				// draw the hidden card
+				Image hiddenCardImage = new ImageIcon(getClass().getResource("cards/BACK.png")).getImage();
+				if (!standButton.isEnabled()) {
+					hiddenCardImage = new ImageIcon(getClass().getResource(hiddenCard.getImagePath())).getImage();
 				}
-				g.drawImage(hiddenCardImg, 20, 20, cardWidth, cardHeight, null);
+				g.drawImage(hiddenCardImage, 20, 20, cardWidth, cardHeight, null);
 
-				// draw dealer's hand
+				// draw the dealer's cards
 				for (int i = 0; i < dealerHand.size(); i++) {
 					Card card = dealerHand.get(i);
-					Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
-					g.drawImage(cardImg, cardWidth + 25 + (cardWidth + 5) * i, 20, cardWidth, cardHeight, null);
+					Image cardImage = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
+					g.drawImage(cardImage, cardWidth + 25 + (cardWidth + 5) * i, 20, cardWidth, cardHeight, null);
 				}
 
-				// draw player's hand
+				// draw the player's cards
 				for (int i = 0; i < playerHand.size(); i++) {
 					Card card = playerHand.get(i);
-					Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
-					g.drawImage(cardImg, 20 + (cardWidth + 5) * i, 320, cardWidth, cardHeight, null);
+					Image cardImage = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
+					g.drawImage(cardImage, 20 + (cardWidth + 5) * i, 320, cardWidth, cardHeight, null);
 				}
 
-				if (!stayButton.isEnabled()) {
-					dealerSum = reduceDealerAce();
-					playerSum = reducePlayerAce();
+				//handle game ending and player's bets
+				if (!standButton.isEnabled()) {
+					dealerHandSum = reduceDealerAce();
+					playerHandSum = reducePlayerAce();
 
 					String message = "";
-					if (playerSum > 21) {
-						message = "You busted!";
-					} else if (dealerSum > 21) {
-						message = "Dealer busted, You Win!";
-					}
-					// both you and dealer <= 21
-					else if (playerSum == dealerSum) {
-						message = "Pushed!";
-					} else if (playerSum > dealerSum) {
-						message = "You Win!";
-					} else if (playerSum < dealerSum) {
-						message = "You Lose!";
+					
+					//both player and user have blackjack
+					if(playerHandSum == 21 && dealerHandSum == 21){
+						message = "Pushed! " + playerBet + " has been returned";
+					} //player has blackjack and the dealer does not 
+					else if(playerHandSum == 21 && dealerHandSum != 21) {
+						message = "You have Blackjack! You won " + ((playerBet*3)/2) + " chips";
+						raisePlayerChips(playerBet, 1.5);
+					} //dealer has blackjack and the player does not 
+					else if(dealerHandSum == 21 && playerHandSum != 21) {
+						message = ("Dealer has Blackjack! You lost " + playerBet + " chips");
+						lowerPlayerChips(playerBet);
+					} //player busts 
+					else if(playerHandSum > 21) {
+						message = "You busted! You lost " + playerBet + " chips";
+						lowerPlayerChips(playerBet);
+					} //dealer busts 
+					else if(dealerHandSum > 21) {
+						message = "Dealer busted! You won " + playerBet + " chips";
+						raisePlayerChips(playerBet, 1);
+					}  //player has a greater hand than the dealer
+					else if(playerHandSum > dealerHandSum) {
+						message = "You won " +  playerBet + " chips";
+						raisePlayerChips(playerBet, 1);
+					} //dealer has a greater hand than the dealer
+					else if(dealerHandSum > playerHandSum) {
+						message = "You lost " + playerBet + " chips";
+						lowerPlayerChips(playerBet);
 					}
 
 					g.setFont(new Font("Arial", Font.PLAIN, 30));
@@ -129,9 +147,8 @@ public class BlackJack {
 	};
 	JPanel buttonPanel = new JPanel(); // create a panel for the buttons
 	JButton hitButton = new JButton("Hit"); // create a button for the player to hit
-	JButton stayButton = new JButton("Stay"); // create a button for the player to stay
-	JTextField betField = new JTextField(10); // create a text field so the user can input a bet
-	JButton setBetButton = new JButton("Enter Bet"); // create a button that confirms the user's bet
+	JButton standButton = new JButton("Stand"); // create a button for the player to stay
+	JLabel chipsIndicator = new JLabel("Current total: " + playerChips + " Current Bet: " + playerBet);
 	JButton playAgainButton = new JButton("Play Again"); //create a button so you can play again
 
 	BlackJack() {
@@ -148,19 +165,20 @@ public class BlackJack {
 		gamePanel.setLayout(new BorderLayout());
 		gamePanel.setBackground(new Color(53, 101, 77));
 		frame.add(gamePanel);
-
+		System.out.println("In graphics");
 		// set attributes for the hit, stay, and setBet button and the bet text field
 		hitButton.setFocusable(false);
 		buttonPanel.add(hitButton);
-		stayButton.setFocusable(false);
-		buttonPanel.add(stayButton);
+		standButton.setFocusable(false);
+		buttonPanel.add(standButton);
+		buttonPanel.add(chipsIndicator);
 		frame.add(buttonPanel, BorderLayout.SOUTH);
 
 		// handle when the player presses the hit button
 		hitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Card card = deck.remove(deck.size() - 1); // get the card the player will get from hitting
-				playerSum += card.getValue(); // get the value of the card
+				playerHandSum += card.getValue(); // get the value of the card
 				playerAceCount += card.isAce() ? 1 : 0; // check if the card is an ace if so increase playerAceCount by 1
 				playerHand.add(card); // add the card to the player's hand
 				if (reducePlayerAce() > 21) { // if the player is over 21 but has an ace reduce the value from 11 to 1
@@ -171,14 +189,14 @@ public class BlackJack {
 		});
 
 		// handle when the player presses the stay button
-		stayButton.addActionListener(new ActionListener() {
+		standButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				hitButton.setEnabled(false);
-				stayButton.setEnabled(false);
+				standButton.setEnabled(false);
 
-				while (dealerSum < 17) { // the dealer will only get another card if their card total is below 17
+				while (dealerHandSum < 17) { // the dealer will only get another card if their card total is below 17
 					Card card = deck.remove(deck.size() - 1); // get the card the dealer will get
-					dealerSum += card.getValue(); // get the value of the card
+					dealerHandSum += card.getValue(); // get the value of the card
 					dealerAceCount += card.isAce() ? 1 : 0; // check if the card is an ace if so increase dealerAceCount by 1
 					dealerHand.add(card); // add the card to the dealer's hand
 				}
@@ -191,35 +209,36 @@ public class BlackJack {
 
 	public void startGame() {
 		// deck
-		buildDeck(); // build the deck of cards
-		shuffleDeck(); // shuffle the deck of cards
-		getUserBet(getPlayerChips()); //get the user's bet for the hand
+		buildDeck(); //build the deck of cards
+		shuffleDeck(); //shuffle the deck of cards
+		setPlayerChips(); //set the players total chips before the hand
+		setUserBet(); //set the user's bet for the hand
 
 		// dealer
 		dealerHand = new ArrayList<Card>();
-		dealerSum = 0; // initialize the sum of the dealer's cards
+		dealerHandSum = 0; // initialize the sum of the dealer's cards
 		dealerAceCount = 0; // initialize the count of aces in the dealer's hand
 
 		// get the dealer's hidden card
 		hiddenCard = deck.remove(deck.size() - 1); // remove card at last index
-		dealerSum += hiddenCard.getValue();
+		dealerHandSum += hiddenCard.getValue();
 		dealerAceCount += hiddenCard.isAce() ? 1 : 0;
 
 		// get the dealer's one face up card
 		Card card = deck.remove(deck.size() - 1);
-		dealerSum += card.getValue();
+		dealerHandSum += card.getValue();
 		dealerAceCount += card.isAce() ? 1 : 0;
 		dealerHand.add(card);
 
 		// player
 		playerHand = new ArrayList<Card>();
-		playerSum = 0; // initialize the sum of the player's cards
+		playerHandSum = 0; // initialize the sum of the player's cards
 		playerAceCount = 0; // initialize the count of aces in the player's hand
 
 		// give the player their two starting cards
 		for (int i = 0; i < 2; i++) {
 			card = deck.remove(deck.size() - 1);
-			playerSum += card.getValue();
+			playerHandSum += card.getValue();
 			playerAceCount += card.isAce() ? 1 : 0;
 			playerHand.add(card);
 		}
@@ -250,15 +269,16 @@ public class BlackJack {
 		}
 	}
 	
-	private int getPlayerChips() {
+	public void setPlayerChips() {
+		playerChips = 50000;
+		System.out.println("Player chips: " + playerChips);
 		
-		return 50000;
 	}
 
 	//get the user's bet
-	public void getUserBet(int totalChips) {
+	public void setUserBet() {
 	    while (true) {
-	        String betFieldText = JOptionPane.showInputDialog(null, "Enter your bet (Remaing chips: " + totalChips + "):");
+	        String betFieldText = JOptionPane.showInputDialog(null, "Enter your bet (Remaing chips: " + playerChips + "):");
 
 	        try {
 	            if (betFieldText == null || betFieldText.trim().isEmpty()) {
@@ -269,36 +289,48 @@ public class BlackJack {
 	            playerBet = Integer.parseInt(betFieldText);
 
 	            // Check if the parsed number is within the valid range
-	            if (playerBet < 10 || playerBet > 50000) {
+	            if (playerBet < 10 || playerBet > 50000 || playerBet > playerChips) {
 	                throw new NumberFormatException();
 	            }
 	            // If everything is valid, break out of the loop
 	            break;
 	        } catch (NumberFormatException nfe) {
 	            // Handle if the input is empty, not a number, or outside the valid range
-	            JOptionPane.showMessageDialog(null, "Invalid Bet: Please enter a number between 10 and 50000",
+	            JOptionPane.showMessageDialog(null, "Invalid Bet: Please enter a number between 10 and 50000 and it must be under " + playerChips,
 	                    "Invalid Bet", JOptionPane.ERROR_MESSAGE);
 	        }
 	    }
-	    System.out.println(playerBet);
+	    System.out.println("Player1 bet " + playerBet + " chips");
+	}
+	
+	public void lowerPlayerChips(int playerBet) {
+		System.out.println("Player chips before lowering " + playerChips);
+		playerChips -= playerBet;
+		System.out.println("Player chips after lowering " + playerChips);
+	}
+	
+	public void raisePlayerChips(int playerBet, double betModifier) {
+		System.out.println("Player chips before raising " + playerChips);
+		playerChips += (playerBet * betModifier);
+		System.out.println("Player chips after raising " + playerChips);
 	}
 
 
 	// method to reduce the player's score if they go over 21 and have an ace in their hand
 	public int reducePlayerAce() {
-		while (playerSum > 21 && playerAceCount > 0) {
-			playerSum -= 10;
+		while (playerHandSum > 21 && playerAceCount > 0) {
+			playerHandSum -= 10;
 			playerAceCount -= 1;
 		}
-		return playerSum;
+		return playerHandSum;
 	}
 
 	// method to reduce the dealer's score if they go over 21 and have an ace in their hand
 	public int reduceDealerAce() {
-		while (dealerSum > 21 && dealerAceCount > 0) {
-			dealerSum -= 10;
+		while (dealerHandSum > 21 && dealerAceCount > 0) {
+			dealerHandSum -= 10;
 			dealerAceCount -= 1;
 		}
-		return dealerSum;
+		return dealerHandSum;
 	}
 }
